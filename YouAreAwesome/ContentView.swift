735f1d9d52db4cc5030222ac62fef27fb5595eef
gallaugher <john.gallaugher@bc.edu>
2022-07-22 16:28:24 -0400

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//  Created by John Gallaugher on 7/22/22
+//  YouTube: YouTube.com/profgallaugher, Twitter: @gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,16 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//  Created by John Gallaugher on 7/22/22
+//  YouTube: YouTube.com/profgallaugher, Twitter: @gallaugher
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
